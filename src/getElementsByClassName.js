686653081f74d60eda8a93a0c18acabcab78cfb4
Hyungjin Lee <leehyungjin4u@gmail.com>
2019-10-14 16:47:08 -0700

    Write getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,4 +7,16 @@
 var getElementsByClassName = function(className
 ) {
   // your code here
+  let coll = document.getElementsByTagName("*");
+  let list = Array.prototype.slice.call(coll, 0);
+  let returnList = [];
+  for(let i = 0; i < list.length; i++){
+    let names = list[i].className.split(' ');
+    for(let j = 0; j < names.length; j++){
+      if(names[j] === className){
+        returnList.push(list[i]);
+      }
+    }
+  }
+  return returnList;
 };
